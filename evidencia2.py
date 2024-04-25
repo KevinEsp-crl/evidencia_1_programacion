2967cc254f12894115f0ead924eee62b3156cfb0
KevinEsp-crl <160693113+KevinEsp-crl@users.noreply.github.com>
2024-04-25 16:51:42 -0600

    Add files via upload

---
+++ evidencia2.py
@@ -0,0 +1,29 @@
+class  Animal:
+    def__init__(self, nombre, especie):
+
+    self.nombre= nombre
+    self.especie= especie
+
+    def get_nombre(self):
+        return self.nombre
+
+        def set_nombre(self,nombre):
+            self.nombre= nombre 
+
+            def get_especie(self):
+                return self.especie 
+
+                def set_especie(self,especie):
+                    self.especie
+#metodo
+                    def hacer_sonido(self):
+                        pass
+
+                    def__str__(self):
+                    return f"{self.nombre} ({self.especie})"
+
+from animal import Animal
+
+class Leon(Animal):
+    def__init__(self,nombre):
+    self 
